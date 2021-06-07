787aa3644d2403a9133630757c373f94e3daee8d
Acheampong Nana <achiekobby17@gmail.com>
2021-06-07 18:07:43 +0000

    Navigation for mobile view has been added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 // code to show the menu
 const toggleBtn = document.getElementById("nav-toggle");
 const navMenu = document.getElementById("nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section[id]");
 
 const showMenu = () => {
     if(toggleBtn && navMenu){
@@ -11,3 +13,32 @@ const showMenu = () => {
 }
 showMenu();
 // end of code to show mobile view navigation menu
+
+// code to remove the show menu class whenever a link is clicked
+const clickLink = ()=>{
+    navMenu.classList.remove("show-menu");
+}
+navLinks.forEach(n=>n.addEventListener('click',clickLink));
+// code to remove the show menu class whenever a link is clicked ends
+
+//scroll sections active links 
+
+
+const activeOnScroll = ()=>{
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(currentSection=>{
+        const sectionHeight = currentSection.offsetHeight;
+        const sectionTop = currentSection.offsetTop-50;
+        sectionId  = currentSection.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY>=sectionHeight +sectionTop){
+            document.querySelector('.nav-menu a[href*='+sectionId+']').classList.add('active-link');
+        }
+        else{
+            document.querySelectorAll('.nav-menu a[href=*'+sectionId+']').classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll', activeOnScroll);
+//scroll sections active links ends here 
