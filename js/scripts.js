f9d019a9c84ad96245ca1dd2d6b785b021484fd3
Acheampong Nana <achiekobby17@gmail.com>
2021-06-08 18:11:08 +0000

    Mobile View home section has been designed and added

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,16 @@ const toggleBtn = document.getElementById("nav-toggle");
 const navMenu = document.getElementById("nav-menu");
 const navLinks = document.querySelectorAll(".nav-link");
 const sections = document.querySelectorAll("section[id]");
+const mobileMenu = document.getElementById("mobile-menu");
+const headerNav = document.getElementById("header");
+const scrollTopBtn = document.getElementById("scrollTop");
 
 const showMenu = () => {
     if(toggleBtn && navMenu){
         toggleBtn.addEventListener('click',()=>{
             navMenu.classList.toggle('show-menu');
+            mobileMenu.classList.toggle("bx-exit");
+            mobileMenu.classList.toggle("bx-menu");
         })
     }
 }
@@ -32,13 +37,34 @@ const activeOnScroll = ()=>{
         const sectionTop = currentSection.offsetTop-50;
         sectionId  = currentSection.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY>=sectionHeight +sectionTop){
+        if(scrollY > sectionTop && scrollY<=sectionHeight +sectionTop){
             document.querySelector('.nav-menu a[href*='+sectionId+']').classList.add('active-link');
         }
         else{
-            document.querySelectorAll('.nav-menu a[href=*'+sectionId+']').classList.remove('active-link');
+            document.querySelector('.nav-menu a[href=*'+sectionId+']').classList.remove('active-link');
         }
     })
 }
 window.addEventListener('scroll', activeOnScroll);
 //scroll sections active links ends here 
+
+// Method to change the header background
+const headerScroll = ()=>{
+    if(this.scrollY >=200){
+        headerNav.classList.add("scroll-header");
+    }
+    else{
+        headerNav.classList.remove("scroll-header");
+    }
+}
+window.addEventListener('scroll',headerScroll);
+
+// Method to toggle display of the scrolltop icon
+const scrollTop = ()=>{
+    if(scrollY >= 560){
+        scrollTopBtn.classList.add("scrollBtn-visible");
+    }
+    else{
+        scrollTopBtn.classList.remove("scrollBtn-visible");
+    }
+}
